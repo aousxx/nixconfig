3c27509c0382599aae9664f1957509d648c596b2
aousxx <33752067+aousxx@users.noreply.github.com>
2023-09-28 11:40:49 +0200

    use mkoutofstoresymlink to enable management of mutable config

--- profiles/aws/configs/default.nix
+++ profiles/aws/configs/default.nix
@@ -1,7 +1,17 @@
+  #home.file.".gitconfig".source if doesnt work
+  # add recursive = true; to each app if it doesnt work too
 { inputs, outputs, lib, config, pkgs, share, ... }:
+
+
+ let
+    link = config.lib.file.mkOutOfStoreSymlink;
+ in
 {
-  xdg.configFile = {
-    MangoHud.source = ./MangoHud;
-    hypr.source = ./hypr;
-  };
+  home.file.".config".source = link ./.config;
+
+  
+  #xdg.configFile = {
+  #    MangoHud.source = ./MangoHud;
+  #    hypr.source = ./hypr;
+  #};
 }
