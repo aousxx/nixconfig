cabb1fd93807b3c544c0670829a872fa1be86787
aousxx <aous.20002017@gmail.com>
2023-12-02 13:42:40 +0100

    ss

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,11 @@
+
+
+
 final: prev:
 
 {
-(lib.mkIf (config.networking.hostName == "aws-lappy"){
-  thorium = prev.callPackage ./thorium-SSE3.nix {};
-});
-(lib.mkIf (config.networking.hostName == "aws"){
+  thorium-SSE3 = prev.callPackage ./thorium-SSE3.nix {};
   thorium = prev.callPackage ./thorium.nix {};
-});
   xwaylandvideobridge = prev.libsForQt5.callPackage ./xwaylandvideobridge.nix {};
   xwaylandvideobridgehypr = final.xwaylandvideobridge.overrideAttrs (o: {
       patches = (o.patches or [ ]) ++ [
