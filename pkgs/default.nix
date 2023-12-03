d9618875c706c247c3a10e2994364db3da025d67
aws abdulrahman <aws.abdulrahman@protonmail.com>
2023-12-03 19:03:34 +0100

    asd

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,21 +2,21 @@ final: prev:
 
 {
  dwl= prev.dwl.overrideAttrs (o: {
-        src = fetchurl {
+        src = final.fetchurl {
           url = "https://codeberg.org/dwl/dwl/archive/v0.5.tar.gz";
-          hash = "118183dl844bm2rc2b0cq9v9vfvcn1fn4240vd590id7bg7pwn9j";
+          hash = "sha256-Mll+z1unRZBK24AIYl2wbLuddsIMLMGyqIsQRNtAAYU=";
         };
         patches = [
-          ./dwl-patches/focusdirection.patch
-          ./dwl-patches/attachbottom.patch
-          ./dwl-patches/monfig.patch
-          ./dwl-patches/point.patch
-          ./dwl-patches/restoreTiling.patch
-          ./dwl-patches/toggleKbLayout.patch
-          ./dwl-patches/cursor_warp.patch
-          ./dwl-patches/output-power-management.patch
-          ./dwl-patches/autostart.patch
-          ./dwl-patches/vanitygaps.patch
+       #   ./dwl-patches/focusdirection.patch
+       #   ./dwl-patches/attachbottom.patch
+       #   ./dwl-patches/monfig.patch
+       #   ./dwl-patches/point.patch
+       #   ./dwl-patches/restoreTiling.patch
+       #   ./dwl-patches/toggleKbLayout.patch
+       #   ./dwl-patches/cursor_warp.patch
+       #   ./dwl-patches/output-power-management.patch
+       #   ./dwl-patches/autostart.patch
+       #   ./dwl-patches/vanitygaps.patch
         ];
         });
   thorium-SSE3 = prev.callPackage ./thorium-SSE3.nix {};
