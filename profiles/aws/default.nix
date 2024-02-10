5f362cf78e819cb52159089442157a9ab3f29561
aousxx <33752067+aousxx@users.noreply.github.com>
2024-02-10 10:29:03 +0100

    Update default.nix

--- profiles/aws/default.nix
+++ profiles/aws/default.nix
@@ -17,38 +17,46 @@
   };
 
   home.packages = with pkgs; [
-    ueberzugpp
-    piper-tts
-    wf-recorder     slurp
-# whisper-ctranslate2
+    
+    # whisper-ctranslate2
+    
     unstable.llama-cpp ollama
     rocmPackages.rocminfo     pciutils
-    lf
-    mpvpaper
-   #ttf-icomoon-feather
-    bottles
-    xwaylandvideobridgehypr
-    wget winetricks wine-staging android-tools  xorg.xhost xdg-desktop-portal-gtk at-spi2-atk 
-     
-    glib flavours 
-    #wpgtk 
-    ventoy-full
-    rustup nodejs_20 (pkgs.python3.withPackages (ps: with ps; [virtualenv psutil pipx])) 
-    imagemagick kitty fzf  gh  git-lfs git  playerctl  zsh  tldr  autojump  thefuck
-    neovim
-    vulkan-tools  wine  protonup-qt  lutris  mangohud  gamemode  #gamescope
-    zellij
-
-    kitty 
-
-    kdenlive
     
-    mimic jellyfin-ffmpeg feh vlc  mpv  syncplay
+
+    bottles  heroic vulkan-tools  wine  protonup-qt  lutris  mangohud  gamemode
+    winetricks wine-staging android-tools  xorg.xhost xdg-desktop-portal-gtk at-spi2-atk 
+
+    xwaylandvideobridgehypr
+
+
+     
+    flavours 
+    
+    ventoy-full
+    
+    
+    neovim zellij kitty 
+    lf imagemagick fzf  gh  git-lfs git  playerctl  zsh  tldr  autojump  thefuck
+
+
+    kdenlive mpvpaper jellyfin-ffmpeg feh vlc  mpv  syncplay
     
     (wrapOBS { plugins = with obs-studio-plugins; [ obs-backgroundremoval obs-vaapi wlrobs obs-gstreamer obs-vkcapture ]; })
+    
+
     discord telegram-desktop  signal-desktop 
-    go gcc yt-dlp spotify-tui thorium microsoft-edge  gimp
-    heroic
+
+    rustup nodejs_20 (pkgs.python3.withPackages (ps: with ps; [virtualenv psutil pipx])) 
+    go gcc glib wget  
+
+
+    ueberzugpp
+    piper-tts
+    wf-recorder slurp yt-dlp spotify-tui 
+
+    thorium gimp
+    
     
     #sound :
     pavucontrol  qpwgraph wireplumber  helvum
@@ -56,22 +64,18 @@
     #hypr assets : 
     swayidle way-displays  polkit_gnome  cliphist wl-clipboard wtype libsForQt5.dolphin  inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
     libsForQt5.ark waybar  gnome.gnome-software bemenu  wpa_supplicant    wlr-randr libsForQt5.qt5.qtwayland  mako  swww  connman-gtk  blueberry gparted   
-
-    #wofi
-     
-    
+ 
     flatpak  flatpak-builder
     
     #fonts
-
     jetbrains-mono  font-awesome  font-awesome_5  font-awesome_4  #nerdfonts
     terminus-nerdfont  inconsolata-nerdfont  nerdfix  fira-code  fira-code-symbols
     
     #LSPs
     gopls nil nodePackages."@tailwindcss/language-server" 
-    atool 
+     
     #atool depends
-    zip unzip xz unrar p7zip gzip
+    atool zip unzip xz unrar p7zip gzip
   ];
 
   
